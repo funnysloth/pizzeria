0bdeebb924ee9a5909f0e3133961fab5569865cc
ArhipovDima <Arhipovdmitroas@gmail.com>
2022-06-07 21:32:46 +0300

    update

--- src/main/java/com/diploma/pizzeria/controllers/PizzaOfFourController.java
+++ src/main/java/com/diploma/pizzeria/controllers/PizzaOfFourController.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -32,7 +33,7 @@ public class PizzaOfFourController {
     }
 
     @GetMapping("/pizza-of-four")
-    public String pizzaOfFour(Model model, HttpServletRequest request, HttpSession session){
+    public String pizzaOfFour(Model model, HttpSession session){
         model.addAttribute("user", session.getAttribute("user"));
         model.addAttribute("title", "Головна сторінка");
         model.addAttribute("categories", categoriesService.getAllCategories());
--- src/main/resources/static/js/element.js
+++ src/main/resources/static/js/element.js
@@ -59,4 +59,29 @@ $(document).ready(function($) {
         e.preventDefault();
         $('html, body').animate({scrollTop:0}, '300');
     });
+
+    $(".select-pizza").change(function () {
+        if(!$(".add-to-cart-pizza").prop("disabled")) {
+            let share = $(".share-container");
+            share.show();
+            share.css("display", "flex");
+            let first_dish = $("#first-part option:selected").data("id");
+            let second_dish = $("#second-part option:selected").data("id");
+            let third_dish = $("#third-part option:selected").data("id");
+            let fourth_dish = $("#fourth-part option:selected").data("id");
+            let url = "localhost:8080/pizza-of-four?pizzaOfFour=" + first_dish  + "," + second_dish
+                + "," + third_dish + "," + fourth_dish;
+            $(".share-tg").prop("href", "https://t.me/share/url?url=" + url);
+            $(".share-fb").prop("href", "https://www.facebook.com/sharer/sharer.php/url?url=" + url);
+            $(".share-twitter").prop("href", "https://twitter.com/intent/tweet?url=" + url);
+        }
+    })
+
+    $(".copy-link-btn").click(function () {
+        navigator.clipboard.writeText("localhost:8080/pizza-of-four?pizzaOfFour=" + $("#first-part option:selected").data("id") + ","
+            + $("#second-part option:selected").data("id") + "," + $("#third-part option:selected").data("id") + ","
+            + $("#fourth-part option:selected").data("id")).then(function () {
+            
+        })
+    })
 })
